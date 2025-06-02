be3d3e509174615eb9faf18d92ff70fa5499c890
Zubair Ansari <ansarizubair3101@gmail.com>
2025-06-02 13:06:28 +0530

    Initial Commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+function Login() {
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+  let [isError, setIsError] = useState(false);
+  let [loginFlag, setLoginFlag] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username !== "user" && password !== "password") {
+      setIsError(true);
+      setLoginFlag(false);
+    } else if (username === "user" && password === "password") {
+      setLoginFlag(true);
+      setUsername("");
+      setPassword("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Form</h1>
+      {loginFlag ? (
+        <p>Welcome, User!</p>
+      ) : (
+        <>
+          {isError && <p>Invalid Username or Password</p>}
+          <form action="submit">
+            <div className="username">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              {console.log(username)}
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              {console.log(password)}
+            </div>
+            <button type="submit" onClick={(e) => handleSubmit(e)}>
+              Submit
+            </button>
+          </form>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Login;
